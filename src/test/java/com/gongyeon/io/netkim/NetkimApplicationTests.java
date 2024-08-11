cd5df56f1537661162b89520d8b66efc902b4519
박창빈 <changbin1528@naver.com>
2024-08-12 00:30:06 +0900

    [CB] Set : Finish Basic Set
      - Spring JPA, DB Connection 설정 완료
      - Spring Security 사용 설정 완료
      - Swagger(Spring Docs) 설정 완료

---
+++ src/test/java/com/gongyeon/io/netkim/NetkimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gongyeon.io.netkim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetkimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
